d54c7e4cc4fe0c48a844b06165cfc5f5046c0ab8
Evgeniy-Sub8way <sub8way@gmail.com>
2025-12-30 15:42:28 +0200

    final

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,11 @@ function getShippingCost(country) {
             break;
         case "Chile": message = `Shipping to ${country} will cost 250 credits`;
             break;
-        case "Australia": message = `Shipping to ${country} will cost 170 credits`; break; case "Jamaica": message = `Shipping to ${country} will cost 120 credits`; break; default: message = "Sorry, there is no delivery to your country";
+        case "Australia": message = `Shipping to ${country} will cost 170 credits`;
+            break;
+        case "Jamaica": message = `Shipping to ${country} will cost 120 credits`;
+            break;
+        default: message = "Sorry, there is no delivery to your country";
     } return message;
 }
 
